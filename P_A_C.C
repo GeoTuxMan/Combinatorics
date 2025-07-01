e9f7eb1469a509e8754921543af9208ce82a368e
Gheorghe Sarbu <ghesarbu@gmail.com>
2025-07-01 15:08:33 +0300

    Add files via upload

---
+++ P_A_C.C
@@ -0,0 +1,43 @@
+/* permutari de k */
+/* aranjamente de n luate cate k */
+/* combinari de n luate cate k */
+// k <= n
+
+#include <conio.h>
+#include <stdio.h>
+
+long fact(int m);
+
+unsigned n,k;
+double p,a,c;
+
+int main()
+{
+  
+  printf("n= ");
+  scanf("%u",&n);
+  printf("k= ");
+  scanf("%u",&k);
+  p=fact(k);
+  a=fact(n)/fact(n-k);
+  c=fact(n)/(fact(k)*fact(n-k));
+  printf("\n\nPermutations(%d)=%3.2f\n\n",n,p);
+  printf("Arrangements(%d,%d)=%3.2f\n\n",n,k,a);
+  printf("Combinations(%d,%d)=%3.2f\n\n",n,k,c);
+  
+  return 0;
+}
+
+long fact(int m)
+{
+ int i,t;
+ if(m==0)
+  return 1;
+ else
+ {
+  t=1;
+  for(i=1;i<=m;i++)
+   t*=i;
+ }
+ return t;
+}
